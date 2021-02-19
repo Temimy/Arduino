c98a8cae43485c6171a49b807d480f94e4628e2f
Temimy <69554542+Temimy@users.noreply.github.com>
2021-02-19 15:09:39 +0300

    Add files via upload

--- Blink_led_Bush_Botton/Blink_led_Bush_Botton.ino
+++ Blink_led_Bush_Botton/Blink_led_Bush_Botton.ino
@@ -5,7 +5,7 @@
 
 Turns an external LED on/off with bush Button.
 
-Tools : 2V Led , 220 Ohms Resistance (R1) , Bush button Switch (S1)
+Tools : 2V Led , 220 Ohms Resistance (R1) , Push Button Switch (S1)( Normal closed)
 
 Connections :
             * Pin 9 - R1 - Led (Anode)... Led (Cathod) - GND *
@@ -15,50 +15,43 @@ Note: Allways check carfully the wires, devices and sensors
       connections before connect the Arduino to the computer 
 
 created by DR. Alaa Temimy
-               18 Feb. 2021
+               19 Feb. 2021
 */
  
 
 // Define pin numbers used for the application
 const int LED = 9; // LED SIGNAL OUTPUT PIN NUMBER
 const int BUTTON = 2; // BUTTON SIGNAL INPUT PIN NUMBER
-
-// Define boolean function to trace led status when bush button is pressed
-boolean cBtn = LOW;   // current statuse of button
-boolean beforeBtn = LOW;  // before status of button
-boolean btnStatus = LOW;  // monitoring status
+int Tmi =500 ; // Re-pushing Button time interval
+int cBtn = 0 ; // current statuse of button
 boolean ledStatus = true; // led statuse at start
-
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  
+  pinMode (BUTTON , INPUT); // Initialize digital pin BUTTON as an input
   pinMode(LED, OUTPUT); // Initialize digital pin LED as an output.
+  digitalWrite(LED, ledStatus); // turn led on at starting
   pinMode (BUTTON , INPUT); // Initialize digital pin BUTTON as an input.
-}
-
-// variable function to return button status
-
-boolean checkBtnStatus(boolean lastUpdate){  // lastUpdate is the input signal
-  boolean lastStatus = digitalRead(BUTTON);  // lastStatus is the saved signal 
-  if (lastUpdate != lastStatus ){
-    lastStatus = digitalRead(BUTTON);
-    }
- 
-  return lastStatus;
-   }
+  Serial.begin (9600);
+        }
 
 // the loop function runs over and over again forever
 void loop() {
-  // read button status  before pressing
-   beforeBtn=  checkBtnStatus(cBtn);
+  // read button status
+    cBtn= digitalRead(BUTTON);
+    Serial.println (cBtn);
+   // delay (1000);
 
-  if ( cBtn == LOW && beforeBtn == HIGH){
+  if ( cBtn == 1){  // Button is pushed
       ledStatus = !ledStatus; // changing led statues False / True
+      digitalWrite(LED, ledStatus);    // turn the LED ON/off based on True / False
+      
         } 
-  cBtn = beforeBtn; 
+        
+      delay (Tmi);
   
-  digitalWrite(LED, ledStatus);    // turn the LED ON/off based on True / False
+  
+ 
 }
 
 
